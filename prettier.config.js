0fea8133234a30014640cb2a813f29ddf4cda0d4
greeenboi <suvan.gowrishanker.204@gmail.com>
2025-01-30 17:25:49 +0530

    chore: update prettier configuration to ignore specific patterns and enhance supabase client type safety

--- prettier.config.js
+++ prettier.config.js
@@ -4,7 +4,7 @@ module.exports = {
   singleQuote: true,
   bracketSameLine: true,
   trailingComma: 'es5',
-
+  ignorePatterns: ['**/node_modules/**', '**/.next/**', '**/.vercel/**', 'database.types.ts'],
   plugins: [require.resolve('prettier-plugin-tailwindcss')],
   tailwindAttributes: ['className'],
 };
